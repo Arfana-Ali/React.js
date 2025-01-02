dbe8b81f9557ba28ea44af2297c8c2e80d325754
Arfana_ali <152651063+Webplorer@users.noreply.github.com>
2025-01-02 21:12:05 +0530

    Update MyComponent.jsx

--- React-Learn/12-update-array/MyComponent.jsx
+++ React-Learn/12-update-array/MyComponent.jsx
@@ -1,1 +1,38 @@
 
+import { useState } from "react";
+function MyComponent() {
+  const [foods, setFoods] = useState(["apple", "mango", "banana"]);
+  function handleAddFood() {
+    const addFood = document.getElementById("addFood").value;
+    document.getElementById("addFood").value = "";
+    setFoods((f) => [...f, addFood]);
+  }
+  function handleRemoveFood() {
+    const removeFood = document.getElementById("removeFood").value;
+    document.getElementById("removeFood").value = "";
+    const foodAfterRemove = foods.filter((ele) => ele !== removeFood);
+    setFoods(foodAfterRemove);
+  }
+  function handleRemoveItem(index) {
+    setFoods(foods.filter((_, i) => i !== index));
+  }
+  return (
+    <div>
+      <h1>Hello In our Restaurent</h1>
+      <h2>Food Item List</h2>
+      <ul>
+        {foods.map((food, index) => (
+          <li key={index} onClick={() => handleRemoveItem(index)}>
+            {food}
+          </li>
+        ))}
+      </ul>
+      <input type="text" id="addFood" placeholder="Enter Food Item" />
+      <button onClick={handleAddFood}>Add Food</button>
+      <br />
+      <input type="text" id="removeFood" placeholder="Remove Food Item" />
+      <button onClick={handleRemoveFood}>Remove Food</button>
+    </div>
+  );
+}
+export default MyComponent;
