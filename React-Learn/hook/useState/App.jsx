227627d2321423aec32ab3dc35a5970f7e95177d
Arfana_ali <152651063+Arfana-Ali@users.noreply.github.com>
2025-02-22 14:15:30 +0530

    Update App.jsx

--- React-Learn/hook/useState/App.jsx
+++ React-Learn/hook/useState/App.jsx
@@ -1,8 +1,23 @@
+// for UseState.jsx
 import UseState from "./UseState";
 function App() {
   return (
     <div>
       <UseState />
+    </div>
+  );
+}
+export default App;
+
+// for UseState2.jsx
+
+
+
+import UseState2 from "./UseState";
+function App() {
+  return (
+    <div>
+      <UseState2 />
     </div>
   );
 }
